27725e60b66c7d11f77ad4ab84426a904d154c2f
Samuel <samlcharreyron@gmail.com>
2019-06-12 11:48:06 +0200

    single function for testing gradients

---
+++ test_gradients.m
@@ -0,0 +1,122 @@
+function test_gradients( model_name, grid, noise_std)
+% grid = struct('size', {3,4,5,6}, 'degree', {3, 4, 4, 4});
+
+    for g=1:length(grid)
+
+        grid_size = grid(g).size;
+        
+        fprintf('running grid size: %dx%d\n', grid_size, grid_size);
+
+        nodes_dataset = sprintf('/Volumes/msrl/users/samuelch/datasets/cmag_calibration/mpem_synthetic_%d_h5/', grid_size);
+        EVAL_DATASET = '/Volumes/msrl/users/samuelch/datasets/cmag_calibration/mpem_synthetic_16_h5/';
+
+        % load currents file
+        %currents = h5read('/Volumes/msrl/users/samuelch/datasets/cmag_calibration/currents_3787.h5', '/currents')';
+
+        % load positions
+        nodes_pos_fn = fullfile(nodes_dataset,'/positions.h5');
+        xg = h5read(nodes_pos_fn, '/xg');
+        yg = h5read(nodes_pos_fn, '/yg');
+        zg = h5read(nodes_pos_fn, '/zg');
+
+        xg = permute(xg, [3, 2, 1]);
+        yg = permute(yg, [3, 2, 1]);
+        zg = permute(zg, [3, 2, 1]);
+
+%         xd = xg(:);
+%         yd = yg(:);
+%         zd = zg(:);
+
+        nodes = cat(4, xg, yg, zg);
+
+        % load positions
+        eval_pos_fn = fullfile(EVAL_DATASET,'/positions.h5');
+
+        xg_ev = h5read(eval_pos_fn, '/xg');
+        yg_ev = h5read(eval_pos_fn, '/yg');
+        zg_ev = h5read(eval_pos_fn, '/zg');
+
+        xg_ev = permute(xg_ev, [3, 2, 1]);
+        yg_ev = permute(yg_ev, [3, 2, 1]);
+        zg_ev = permute(zg_ev, [3, 2, 1]);
+
+        positions_ev = cat(4, xg_ev, yg_ev, zg_ev);
+
+        NUM_CURRENTS = 20;
+
+        mae = zeros(NUM_CURRENTS, 3, 3);
+        r2 = zeros(NUM_CURRENTS, 3, 3);
+        meandivs = zeros(NUM_CURRENTS, 1);
+        meancurls = zeros(NUM_CURRENTS, 3);
+
+
+        for i=1:NUM_CURRENTS
+            fields = h5read(fullfile(nodes_dataset, 'v', sprintf('%04d.h5', i)), '/fields');
+            fields = permute(fields, [4, 3, 2, 1]);
+            % adding random noise
+            fields = fields + noise_std * randn(size(fields));
+
+            gradients_ev = h5read(fullfile(EVAL_DATASET, 'v', sprintf('%04d.h5', i)), '/gradients');
+            gradients_ev = permute(gradients_ev, [5, 4, 3, 2, 1]);
+
+            if strcmp(model_name, 'RBF3D')
+                eps = grid(g).eps;
+                model = SimpleRBFInterpolator(nodes, fields, eps);
+            elseif strcmp(model_name, 'RBFD')
+                eps = grid(g).eps;
+                model = DivFreeRBFInterpolator(nodes, fields, eps);
+            elseif strcmp(model_name, 'SPL3D')
+                degree = grid(g).degree;
+                model = BSpline3DInterpolator(nodes, fields, degree);
+            elseif strcmp(model_name, 'SPLLPL')
+                degree = grid(g).degree;
+                model = BSplineLaplacianInterpolator(nodes, fields, degree);
+            elseif strcmp(model_name, 'TRI3D')
+                load('tricubic_simple_M.mat');
+                model = SimpleTricubicInterpolator(nodes, fields, M);
+            elseif strcmp(model_name, 'TRILPL')
+                load('tricubic_scalar_field_matrix.mat');
+                model = TricubicScalarFieldInterpolator(nodes, fields, M);
+            else
+                error('invalid model name');
+            end
+
+            gradients = model.getGradientsAtPositions(positions_ev);
+            divs = sum(gradients(:,:,:,1:3+1:9));
+            grads = reshape(gradients,[],9);
+            curls = [grads(:,6) - grads(:,8), grads(:,7) - grads(:,3), grads(:,2) - grads(:,4)];
+            meancurls(i,:) = mean(curls, 1);
+            meandivs(i) = mean(divs(:));
+
+            mae(i,:,:) = gradmae(gradients_ev, gradients);
+            r2(i,:,:) = gradr2(gradients_ev, gradients);
+
+            percent_done = 100 * i / NUM_CURRENTS;
+
+            if mod(percent_done, 10) == 0
+                fprintf('%d %% done\n', percent_done);
+            end
+
+        end
+
+        save_fn = sprintf('data/gradients/%s_%d_%d.mat', model_name, grid_size, noise_std);
+        save(save_fn, 'mae', 'r2', 'meandivs', 'meancurls');
+
+        disp('');
+        fprintf('grid size: %d\n', grid_size);
+        disp('MAE (T/m):')
+        disp(squeeze(mean(mae, 1)));
+
+        disp('R2:')
+        disp(squeeze(mean(r2, 1)));
+
+        fprintf('Mean divergence: %2.3f (mT/m) \n', 1000*mean(meandivs));
+        temp = 1000*mean(meancurls,1);
+        fprintf('Mean curl: [%2.3f, %2.3f, %2.3f] (mT/m) \n',  temp(1), temp(2), temp(3));
+
+        fprintf('\n\n');
+
+    end
+
+end
+
--- test_gradients_all.m
+++ test_gradients_all.m
@@ -1,17 +1,31 @@
-disp('Testing Simple RBF');
-test_gradients_simple_rbf;
+clear all;
 
-disp('Testing Div-free RBF');
-test_gradients_divfree_rbf;
+RECOMPUTE = 1;
 
-disp('Testing Simple Tricubic');
-test_gradients_simple_tricubic;
+grid_sizes = {3,4,5,6};
 
-disp('Testing Scalar Field Tricubic');
-test_gradient_tricubic_scalar_field;
+if RECOMPUTE ~= 0
+%     disp('Testing RBF 3D');
+%     test_gradients('RBF3D', struct('size', grid_sizes, 'eps', {1., 10., 22., 50.}), 0);
 
-disp('Testing 3D BSpline');
-test_gradients_bspline_3d;
+%     disp('Testing RBF Div-free');
+%     test_gradients('RBFD', struct('size', grid_sizes, 'eps', {35, 35, 50, 50}), 0);
+% 
+    disp('Testing Tricubic 3D');
+    test_gradients('TRI3D', struct('size', grid_sizes), 0);
 
-disp('Testing Laplacian BSpline');
-test_gradients_bspline_laplacian;
+%     disp('Testing Scalar Field Tricubic');
+%     test_gradients('TRILPL', struct('size', grid_sizes), 0);
+% 
+%     disp('Testing 3D BSpline');
+%     test_gradients('SPL3D', struct('size', grid_sizes, 'degree', {3, 4, 4, 4}), 0);
+% 
+%     disp('Testing Laplacian BSpline');
+%     test_gradients('SPLLPL', struct('size', grid_sizes, 'degree', {3, 4, 4, 4}), 0);
+end
+
+output_files = dir('data/gradients');
+
+for i=1:length(output_files)
+    filename = output_files(i);
+end
