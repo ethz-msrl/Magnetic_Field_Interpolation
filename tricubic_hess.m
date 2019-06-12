d6f8e9b3da684ff1860f2fa6fece5f1b4f31a8e3
Samuel <samlcharreyron@gmail.com>
2019-06-12 10:38:16 +0200

    fixed tricubic hessian

--- tricubic_hess.m
+++ tricubic_hess.m
@@ -3,16 +3,16 @@ function [ hess ] = tricubic_hess( A, x, y, z )
 %   Detailed explanation goes here
 
 xv = [x^3, x^2, x, 1]';
-xv1 = [3*x^2, x, 1, 0]';
-xv2 = [6*x, 1, 0, 0]';
+xv1 = [3*x^2, 2*x, 1, 0]';
+xv2 = [6*x, 2, 0, 0]';
 
 yv = [y^3, y^2, y, 1]';
-yv1 = [3*y^2, y, 1, 0]';
-yv2 = [6*y, 1, 0, 0]';
+yv1 = [3*y^2, 2*y, 1, 0]';
+yv2 = [6*y, 2, 0, 0]';
 
 zv = [z^3, z^2, z, 1]';
-zv1 = [3*z^2, z, 1, 0]';
-zv2 = [6*z, 1, 0, 0]';
+zv1 = [3*z^2, 2*z, 1, 0]';
+zv2 = [6*z, 2, 0, 0]';
 
 % av is the vector arrangement of A which is 4x4x4
 % this ensures that the ordering of the parameters of av matches xv
@@ -34,12 +34,11 @@ dgz = [
     kron(kron(xv, yv), zv2)';
     ]';
 
-A = zeros(64, 3, 3);
-A(:,1,:) = dgx;
-A(:,2,:) = dgy;
-A(:,3,:) = dgz;
+H = zeros(64, 3, 3);
+H(:,1,:) = dgx;
+H(:,2,:) = dgy;
+H(:,3,:) = dgz;
 
-hess = squeeze(sum(repmat(av, [1,3,3]) .* A, 1));
+hess = squeeze(sum(repmat(av, [1,3,3]) .* H, 1));
 
 end
-
