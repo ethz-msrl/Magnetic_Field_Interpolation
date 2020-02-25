c8829c1857cb19c0a698958fdbbe5a94be8c5464
Samuel <samlcharreyron@gmail.com>
2020-02-25 17:16:55 +0100

    convenience scripts to load the field and gradient data

---
+++ load_gradient_data.m
@@ -0,0 +1,34 @@
+output_files = dir('data/gradients/*.mat');
+
+load('data/colors');
+load('data/idx');
+load('data/grid_sizes');
+
+Nf = length(output_files);
+
+nmae = zeros(Nf, length(grid_sizes));
+mae = zeros(Nf, length(grid_sizes));
+rmse = zeros(Nf, length(grid_sizes));
+nrmse = zeros(Nf, length(grid_sizes));
+r2 = zeros(Nf, length(grid_sizes));
+mean_div = zeros(Nf, length(grid_sizes));
+mean_curl = zeros(Nf, length(grid_sizes));
+model_names = {};
+
+for i=1:Nf
+    filename = fullfile(output_files(i).folder, output_files(i).name);
+    
+    temp = strsplit(output_files(i).name, '_');
+    model_name = temp{1};
+    model_names{i} = model_name;
+    noise_std = temp{2} * 1e-6;
+    
+    results = importdata(filename);
+    mae(i,:) = mean([results.mae]);
+    nmae(i,:) = mean([results.nmae]);
+    rmse(i,:) = mean([results.rmse]);
+    nrmse(i,:) = mean([results.nrmse]);
+    r2(i,:) = mean([results.r2]);
+    mean_div(i,:) = [results.mean_div];
+    mean_curl(i,:) = sqrt(sum(reshape([results.mean_curl],3,4)'.^2,2));
+end
